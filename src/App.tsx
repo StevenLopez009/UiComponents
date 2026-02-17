5ce8ae47657d0a3967df58759bdddfa5c9384900
Steven Lopez Mancera <150303550+StevenLopez009@users.noreply.github.com>
2026-02-17 12:40:39 -0500

    finalizacion componente WeatherCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,42 @@
-import './App.css'
-import ProfileCard from './components/ProfileCard/ProfileCard'
-import backgroundImage from './assets/images/portada.jpg'
-import backgroundImage2 from './assets/images/portada2.jpg'
-import perfilImage from './assets/images/perfil.jpg'
-import perfilImage2 from './assets/images/perfil2.jpg'
-import OrderTrackingTimeline from './components/OrderTrackingTimeline/OrderTrackingTimeline'
-import WeatherCard from './components/WeatherCard/WeatherCatd'
+import "./App.css";
+import ProfileCard from "./components/ProfileCard/ProfileCard";
+import backgroundImage from "./assets/images/portada.jpg";
+import backgroundImage2 from "./assets/images/portada2.jpg";
+import perfilImage from "./assets/images/perfil.jpg";
+import perfilImage2 from "./assets/images/perfil2.jpg";
+import OrderTrackingTimeline from "./components/OrderTrackingTimeline/OrderTrackingTimeline";
+import WeatherCard from "./components/WeatherCard/WeatherCatd";
 
 function App() {
   return (
-    <div className='app'>
-      <WeatherCard city='London'/>
-      <OrderTrackingTimeline 
+    <div className="app">
+      <WeatherCard />
+      <OrderTrackingTimeline
         order={new Date("2026-02-10")}
         shipping={new Date("2026-02-11")}
         transit={new Date("2026-02-11")}
         sentToCustomer={new Date("2026-03-20")}
       />
-        <ProfileCard 
-          backgroundImage={backgroundImage}
-          imageUrl={perfilImage2}
-          name="Alex Turner"
-          profession="Desarrollador"
-          raiting={5}
-          hours={76}
-          months={2}
-        />
-        <ProfileCard 
-          backgroundImage={backgroundImage2}
-          imageUrl={perfilImage}
-          name="Lilly Gallagher"
-          profession="Desingner"
-          raiting={25}
-          hours={99}
-          months={12}
-        />
+      <ProfileCard
+        backgroundImage={backgroundImage}
+        imageUrl={perfilImage2}
+        name="Alex Turner"
+        profession="Desarrollador"
+        raiting={5}
+        hours={76}
+        months={2}
+      />
+      <ProfileCard
+        backgroundImage={backgroundImage2}
+        imageUrl={perfilImage}
+        name="Lilly Gallagher"
+        profession="Desingner"
+        raiting={25}
+        hours={99}
+        months={12}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/WeatherCard/WeatherCatd.tsx
+++ src/components/WeatherCard/WeatherCatd.tsx
@@ -1,21 +1,37 @@
+import londonImg from "../../assets/images/london.jpg";
+import newYorkImg from "../../assets/images/newyork.jpg";
+import tokyoImg from "../../assets/images/tokio.jpg";
+import sidneyImg from "../../assets/images/sidney.jpg";
+
 import { useEffect, useState } from "react";
-import { getCurrentWeather, type WeatherResponse } from "../../services/weatherService";
+import {
+  getCurrentWeather,
+  type WeatherResponse,
+} from "../../services/weatherService";
+import "./WeatherCard.css";
+import { MapPin, ChevronDown } from "lucide-react";
 
+const cities = ["London", "New York", "Tokyo", "Sydney"];
 
-interface Props {
-  city: string;
-}
+const cityBackgrounds: Record<string, string> = {
+  London: londonImg,
+  "New York": newYorkImg,
+  Tokyo: tokyoImg,
+  Sydney: sidneyImg,
+};
 
-const WeatherCard = ({ city }: Props) => {
+const WeatherCard = () => {
   const [weather, setWeather] = useState<WeatherResponse | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const [selectedCity, setSelectedCity] = useState("London");
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     const fetchWeather = async () => {
       try {
         setLoading(true);
-        const data = await getCurrentWeather(city);
+        const data = await getCurrentWeather(selectedCity);
         setWeather(data);
       } catch (err) {
         setError("No se pudo obtener el clima.");
@@ -25,7 +41,7 @@ const WeatherCard = ({ city }: Props) => {
     };
 
     fetchWeather();
-  }, [city]);
+  }, [selectedCity]);
 
   if (loading) return <p>Loading weather...</p>;
   if (error) return <p>{error}</p>;
@@ -33,19 +49,54 @@ const WeatherCard = ({ city }: Props) => {
   if (!weather) return null;
 
   return (
-    <div className="weather-card">
-      <h2>
-        {weather.location.name}, {weather.location.country}
-      </h2>
+    <div
+      className="weather-card"
+      style={{
+        backgroundImage: `url(${cityBackgrounds[selectedCity]})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="location-select">
+        <MapPin size={16} />
 
-      <h1>{weather.current.temp_c}°C</h1>
+        <div className="custom-select">
+          <div className="select-trigger" onClick={() => setIsOpen(!isOpen)}>
+            <span>{selectedCity}</span>
+            <ChevronDown
+              size={16}
+              className={`chevron ${isOpen ? "open" : ""}`}
+            />
+          </div>
 
-      <img
-        src={weather.current.condition.icon}
-        alt={weather.current.condition.text}
-      />
+          {isOpen && (
+            <div className="dropdown">
+              {cities.map((city) => (
+                <div
+                  key={city}
+                  onClick={() => {
+                    setSelectedCity(city);
+                    setIsOpen(false);
+                  }}
+                >
+                  {city}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+      <h1 className={!isOpen ? "" : "hidden-temp"}>
+        {weather.current.temp_c}°
+      </h1>
 
-      <p>{weather.current.condition.text}</p>
+      <div className="weather-info">
+        <img
+          src={weather.current.condition.icon}
+          alt={weather.current.condition.text}
+        />
+        <p>{weather.current.condition.text}</p>
+      </div>
 
       <div>
         <p>💨 Wind: {weather.current.wind_kph} km/h</p>
